41f75ae0ca02a3216e1e021c01c5527ae2026d89
Martín Rosselot <martin.rosselot@uc.cl>
2024-11-06 01:34:28 -0300

    Tarea 3

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    assetPrefix: process.env.ASSET_PREFIX,
+    basePath: process.env.BASE_PATH,
+};
+
+export default nextConfig;
---
+++ src/app/page.js
@@ -0,0 +1,14 @@
+import Chat from './components/Chat';
+
+export default function Home() {
+  const apiUrl = 'http://34.136.247.4:5000/question'; // Replace with your API endpoint
+
+  return (
+    <div className="h-screen bg-gray-100 flex items-center justify-center">
+      <div className="bg-white shadow-md rounded-lg p-6">
+        <h1 className="text-2xl font-bold mb-4 text-center text-black">Chat Interface</h1>
+          <Chat apiUrl={apiUrl} />
+      </div>
+    </div>
+  );
+}
